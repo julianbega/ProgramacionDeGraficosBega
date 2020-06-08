45bff482ab2861fa4d9fc3ae1ecad361179a3663
BegaJulian <Julian.begamilrad@gmail.com>
2020-06-08 19:24:38 -0300

    Triangle Code

--- Graficos/Graficos/main.cpp
+++ Graficos/Graficos/main.cpp
@@ -1,37 +1,64 @@
-#include <glfw3.h>
-
-int main(void)
+#include<glew.h>
+#include<glfw3.h>
+const GLchar *vertexSource = R"glsl(
+#version 150 core
+in vec2 position;
+void main()
 {
-	GLFWwindow* window;
-
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	/* Make the window's context current */
+gl_Position=vec4(position,0.0,1.0);
+}
+)
+glsl";
+const GLchar* fragmentSource = R"glsl(
+#version 150 core
+out vec4 outColor;
+voidmain()
+{
+outColor=vec4(1.0,0.0,0.0,1.0);
+}
+)glsl";
+int main(int argc, char*argv[])
+{
+	glfwInit();
+	GLFWwindow*window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
 	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
+	glewExperimental = GL_TRUE;
+	glewInit();
+	GLfloat vertices[] =
 	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
+		0.0f,0.5f,
+		0.5f,-0.5f,
+		-0.5f,-0.5f
+	};
+	GLuint vbo;
+	glGenBuffers(1, &vbo); 
+	glBindBuffer(GL_ARRAY_BUFFER, vbo); 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); 
+	glShaderSource(vertexShader, 1, &vertexSource, NULL); 
+	glCompileShader(vertexShader); 
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL); 
+	glCompileShader(fragmentShader); 
+	GLuint shaderProgram = glCreateProgram(); 
+	glAttachShader(shaderProgram, vertexShader); 
+	glAttachShader(shaderProgram, fragmentShader); 
+	glLinkProgram(shaderProgram); 
+	glUseProgram(shaderProgram); 
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position"); 
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0); 
+	glEnableVertexAttribArray(posAttrib); 
+	while (!glfwWindowShouldClose(window))
+	{ 
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			glfwSetWindowShouldClose(window, GL_TRUE); 
 		glfwPollEvents();
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT); glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
 	}
-
-	glfwTerminate();
-	return 0;
+	glDeleteProgram(shaderProgram);
+	glDeleteShader(fragmentShader);
+	glDeleteShader(vertexShader);
+	glDeleteBuffers(1, &vbo);
 }
